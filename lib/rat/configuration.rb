82b0cf42b66c3f08370385d8369f07ca084e1523
elliottcable <git@elliottcable.name>
2008-07-24 16:10:50 -0800

    A lot of work to Rat::Configuration and its children. Much sexier now, though also far more verbose unfortunately. Still probably has a ton of bugs, and needs specing.

--- lib/rat/configuration.rb
+++ lib/rat/configuration.rb
@@ -1,11 +1,141 @@
 module Rat
-  # Contains a single gorified hash, an array of Rat::Configuration instances.
-  # Each instance has a key, an array of categories, and a value of some sort.
-  # TODO: Rename to Rat::Setting, or something similar -
-  # +Rat::Configuration.new+ doesn't really make much semantic sense.
-  class Configuration
+  # Contains a single nested Rat::Configuration::Category (a gorified hash, an
+  # array of Rat::Configuration::Setting) instances. Each instance has a key,
+  # an array of categories, and a value of some sort.
+  module Configuration
+    class Category < Hash
+      # Like Hash#[], but returns the *value* of the object instead of the
+      # object if the object is a Rat::Configuration::Setting
+      def [] key
+        contents = super key
+        return contents.value if contents.is_a?(Rat::Configuration::Setting)
+        contents
+      end
+      
+      # Like Hash#[]=, but sets the *value* of the object instead of the
+      # object if the object is a Rat::Configuration::Setting
+      def []= key, value
+        # Using #values_at, because we've overwritten #[]
+        old_contents = begin
+          self.fetch(key)
+        rescue IndexError
+          nil
+        end
+        if old_contents.is_a? Rat::Configuration::Setting
+           old_contents.value = value
+        else
+          super key, value
+        end
+      end
+    end
+    
+    class Setting
+      # Retrieves either a key or a category
+      def self.[] key
+        Rat::Configuration::settings[key]
+      end
+      
+      # The 'name' of the setting, is used in all references to it. Should be
+      # a Symbol.
+      # 
+      # Can not be changed, because it is used to index the location of the
+      # setting in the configuration.
+      attr_reader :key
+      
+      # An array of categories to which the key belongs - these are
+      # intrepolated as nested, as each setting can only exist in one
+      # location.
+      # 
+      # Can not be changed, because it is used to index the location of the
+      # setting in the configuration.
+      attr_reader :categories
+      
+      # The default value for this setting
+      attr_accessor :default
+      
+      # Sets the value to the setting's default value
+      def default!
+        @is_set = false
+        @value = @default
+      end
+      
+      attr_accessor :value
+      
+      # Sets the value of the setting
+      def value= new_value
+        @is_set = true
+        @value = new_value
+      end
+      
+      # Creates a new configuration variable. Takes a key (like in a normal
+      # hash), and can be given a self-yielding block to set other things
+      # (like a default value or categories).
+      def initialize key
+        key, categories = parse key
+        
+        @key = key
+        @categories = categories
+        
+        @default = nil
+        @value = nil
+        @is_set = false
+        
+        yield self if block_given?
+        
+        cat = category! @categories
+        raise 'there is already a category or setting key by that name' if cat.include? key
+        cat[key] = self
+      end
+      
+      private
+      
+        # Parses a key string or symbol into an actual key and a list of
+        # categories. Takes a string or symbol and any extra categories, and
+        # returns an array of the key and categories.
+        def parse key, precategories = []
+          categories = key.to_s.split "/"
+          key = categories.pop
+          categories = precategories + categories
+
+          [key.to_sym, categories.map! {|c|c.to_sym}]
+        end
+        
+        # Returns a single category (hash) given an array of categories to dig
+        # through for it.
+        def category categories, destructive = false
+          categories.inject(Configuration::settings) do |hash, category|
+            case hash[category]
+            when nil
+              raise "category '#{category}' does not exist" unless destructive
+              hash[category] = Category.new if destructive
+              
+            when Rat::Configuration
+              raise "'#{category}' is a setting key, not a category key"
+              
+            when Hash
+              hash[category]
+              
+            else
+              raise "key has unknown value of class #{hash[category].class}: #{hash[category].inspect}"
+            end
+          end
+        end
+        
+        # Returns a single category (hash) given an array of categories to dig
+        # through for it. Is 'destructive', in that it will create any empty
+        # categories necessary. Will not overwrite setting keys, however.
+        def category! categories
+          category categories, true
+        end
+        
+    end
+    
+    @@settings = Rat::Configuration::Category.new
+    def self.settings
+      @@settings
+    end
+    
     class <<self
-      @@settings = {}
       
       # Dumps current configuration to a file. Doesn't save any default
       # setting, doesn't save any empty categories.
@@ -20,57 +150,7 @@ module Rat
         
       end
       
-      # Retrieves a setting's value, by key and categories
-      # TODO: Implement this
-      def get key, categories = []
-        key, categories = parse key, categories
-
-      end
-      
-      # Retrieves either a key
-      # TODO: Implement this
-      # TODO: Use a special Rat::Configuration::Category class or something 
-      #   instead of the current @@settings hash in order to store
-      #   configuration variables and other categories, so [][][] returns the
-      #   value of the key instead of a setting class instance.
-      def [] key
-        
-      end
     end
-    
-    
-    # Creates a new configuration variable. Takes a key (like in a normal
-    # hash), and can be given a self-yielding block to set other things
-    # (like a default value or categories).
-    def initialize key
-      key, categories = parse key
-      
-      @key = key
-      @categories = categories
-      
-      @default = nil
-      @value = nil
-      @is_set = false
-      
-      yield self if block_given?
-      
-      category = @categories.inject(@@settings) {|hash, category| hash[:category] ||= {} }
-      raise 'there is already a category or setting key by that name' if category.include? key
-      category[key] = self
-    end
-    
-    private
-      
-      # Parses a key string or symbol into an actual key and a list of
-      # categories. Takes a string or symbol and any extra categories, and
-      # returns an array of the key and categories.
-      def parse key, precategories = []
-        categories = key.split "/"
-        key = categories.pop!
-        categories = precategories + categories
-        
-        [key.to_sym, categories.map! {|c|c.to_sym}]
-      end
-      
   end
+
 end
