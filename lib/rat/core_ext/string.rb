02feff3deb895b9c1cc1fc0bdd4e097d9943ab63
elliottcable <git@elliottcable.name>
2008-08-12 19:13:15 -0800

    Fixed #wrap for preformatted line returns

--- lib/rat/core_ext/string.rb
+++ lib/rat/core_ext/string.rb
@@ -42,38 +42,40 @@ class String
     raise ArgumentError, "#{min} is not numeric-ish" unless min.respond_to? :to_i
     
     
-    self.inject([""]) do |wrapped, word|
-      #puts "word: #{word.inspect}, current line: #{wrapped.last.inspect}"
-      # If we're still short enough to fit the word, do so
-      if wrapped.last.length + word.rstrip.length <= width
-        #puts "- new length #{wrapped.last.length + word.rstrip.length} (#{wrapped.last.length} + #{word.rstrip.length}) is less than #{width}\n\n"
-        wrapped.last << word
-      # Else, if we're less than minimum width
-      elsif wrapped.last.length < min
-        #puts "- new length #{wrapped.last.length + word.rstrip.length} (#{wrapped.last.length} + #{word.rstrip.length}) would be more than #{width}"
-        #puts "- current length #{wrapped.last.length} is less than #{min}\n\n"
-        bits = word.split_at(width - wrapped.last.length)
-        wrapped.last << bits.shift
-        bits.join.split_at(width)
-        bits.each {|bit| wrapped << bit}
-      # Else if neither can fit on current line, nor is line short enough; and
-      # the word is short enough to fit on the new line
-      elsif word.chomp.length < width
-        #puts "- new length #{wrapped.last.length + word.rstrip.length} (#{wrapped.last.length} + #{word.rstrip.length}) would be more than #{width}"
-        #puts "- current length #{wrapped.last.length} is more than #{min}"
-        #puts "- word's length #{word.chomp.length} is less than #{width}\n\n"
-        wrapped << word
-      # If it can't fit on the current line, and it can't fit wholly on a line
-      # by it's own
-      else
-        #puts "- new length #{wrapped.last.length + word.rstrip.length} (#{wrapped.last.length} + #{word.rstrip.length}) would be more than #{width}"
-        #puts "- current length #{wrapped.last.length} is more than #{min}"
-        #puts "- word's length #{word.chomp.length} is more than #{width}"
-        bits = word.split_at(width)
-        bits.each {|bit| wrapped << bit}
-      end
+    self.split("\n").map do |line|
+      line.inject([""]) do |wrapped, word|
+        #puts "word: #{word.inspect}, current line: #{wrapped.last.inspect}"
+        # If we're still short enough to fit the word, do so
+        if wrapped.last.length + word.rstrip.length <= width
+          #puts "- new length #{wrapped.last.length + word.rstrip.length} (#{wrapped.last.length} + #{word.rstrip.length}) is less than #{width}\n\n"
+          wrapped.last << word
+        # Else, if we're less than minimum width
+        elsif wrapped.last.length < min
+          #puts "- new length #{wrapped.last.length + word.rstrip.length} (#{wrapped.last.length} + #{word.rstrip.length}) would be more than #{width}"
+          #puts "- current length #{wrapped.last.length} is less than #{min}\n\n"
+          bits = word.split_at(width - wrapped.last.length)
+          wrapped.last << bits.shift
+          bits.join.split_at(width)
+          bits.each {|bit| wrapped << bit}
+        # Else if neither can fit on current line, nor is line short enough; and
+        # the word is short enough to fit on the new line
+        elsif word.chomp.length < width
+          #puts "- new length #{wrapped.last.length + word.rstrip.length} (#{wrapped.last.length} + #{word.rstrip.length}) would be more than #{width}"
+          #puts "- current length #{wrapped.last.length} is more than #{min}"
+          #puts "- word's length #{word.chomp.length} is less than #{width}\n\n"
+          wrapped << word
+        # If it can't fit on the current line, and it can't fit wholly on a line
+        # by it's own
+        else
+          #puts "- new length #{wrapped.last.length + word.rstrip.length} (#{wrapped.last.length} + #{word.rstrip.length}) would be more than #{width}"
+          #puts "- current length #{wrapped.last.length} is more than #{min}"
+          #puts "- word's length #{word.chomp.length} is more than #{width}"
+          bits = word.split_at(width)
+          bits.each {|bit| wrapped << bit}
+        end
       
-      wrapped
+        wrapped
+      end.join("\n")
     end.join("\n")
   end
   
