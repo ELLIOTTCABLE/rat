f127d1e6a2f32a9c01aaac8ca85cfebcb1e55749
elliottcable <git@elliottcable.name>
2008-08-17 03:24:07 -0800

    Lots more specs

--- spec/rat/ncurses/window_wrapper_spec.rb
+++ spec/rat/ncurses/window_wrapper_spec.rb
@@ -124,4 +124,74 @@ describe Ncurses::WindowWrapper do
     end
   end
   
+  describe "#recreate" do
+    before :each do
+      @new_window = mock("another window", :null_object => true)
+    end
+    
+    it "should recreate the window object" do
+      Ncurses.should_receive(:newwin).twice.with(40,80,0,0).and_return(@window, @new_window)
+      wrapper = Ncurses::WindowWrapper.new(40,80,0,0)
+      
+      wrapper.recreate
+    end
+    
+    it "should terminate any existing window object" do
+      Ncurses.should_receive(:newwin).twice.with(40,80,0,0).and_return(@window, @new_window)
+      wrapper = Ncurses::WindowWrapper.new(40,80,0,0)
+      
+      wrapper.should_receive(:terminate)
+      wrapper.recreate
+    end
+    
+    it "should set the new window to keypad" do
+      Ncurses.should_receive(:newwin).twice.with(40,80,0,0).and_return(@window, @new_window)
+      wrapper = Ncurses::WindowWrapper.new(40,80,0,0)
+      
+      @new_window.should_receive(:keypad)
+      wrapper.recreate
+    end
+    
+    it "should refresh the new window" do
+      Ncurses.should_receive(:newwin).twice.with(40,80,0,0).and_return(@window, @new_window)
+      wrapper = Ncurses::WindowWrapper.new(40,80,0,0)
+      
+      @new_window.should_receive(:refresh)
+      wrapper.recreate
+    end
+    
+    it "should accept dimension options" do
+      Ncurses.should_receive(:newwin).twice.and_return(@window, @new_window)
+      wrapper = Ncurses::WindowWrapper.new(40,80,0,0)
+      
+      wrapper.recreate(
+        :height => 10,
+        :width => 72,
+        :top => 1,
+        :left => 1
+      )
+      wrapper.height.should == 10
+      wrapper.width.should == 72
+      wrapper.top.should == 1
+      wrapper.left.should == 1
+    end
+  end
+  
+  describe "#clear" do
+    it "should recreate the window" do
+      Ncurses.should_receive(:newwin).twice.with(40,80,0,0).and_return(@window)
+      wrapper = Ncurses::WindowWrapper.new(40,80,0,0)
+      
+      wrapper.clear
+    end
+    
+    it "should be capable of pushing in existing content" do
+      Ncurses.should_receive(:newwin).twice.with(40,80,0,0).and_return(@window)
+      wrapper = Ncurses::WindowWrapper.new(40,80,0,0)
+      
+      content = "These are some testing strings.\nUse them with care!"
+      @window.should_receive(:printw).with("%s", content)
+      wrapper.clear(content)
+    end
+  end
 end
