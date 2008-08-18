d05287eeb6dd64e08a594d738073513484ae4669
elliottcable <git@elliottcable.name>
2008-08-18 04:28:43 -0800

    Awww, it's cute!

--- lib/rat.rb
+++ lib/rat.rb
@@ -22,23 +22,33 @@ module Rat
   # terminal.
   def self.start
     # First, protocol initialization
-    
-    # for now, let's just get xmpp working. Later, this will pull from
-    # configuration.
-    Protocol::XMPP::initialize
+    # Protocol::Protocols.each {|protocol| protocol::initialize }
     
     # Second, some Ncurses initialization
     Window::initialize
+    rodent = Ncurses::WindowWrapper.new 1, 26, (Ncurses.LINES * 0.25).to_i, (Ncurses.COLS / 2) - 13
+    %w"o/ o\\ o/ o\\ o/".each {|frame| rodent.clear frame + ' '; sleep 0.5 }
+    rodent.clear " (-: "
+    sleep 1
+    rodent.clear " (-: welcome to rat :-) "
+    sleep 2
     
     # Next, we need a few windows.
     Input.new
     window = Window.new :none, :main
-    window << "<:3 )~~~ welcome to rat <:3 )~~~"
     
+    rodent.terminate
     forever do
       Rat::Input.process ::Ncurses.getch
     end
   end
   
-  Command.new(:exit) { exit }
+  Command.new(:exit) do
+    rodent = Ncurses::WindowWrapper.new 1, 26, (Ncurses.LINES * 0.25).to_i, (Ncurses.COLS / 2) - 13
+    %w"o/ o\\ o/ o\\ o/".each {|frame| rodent.clear frame + ' '; sleep 0.5 }
+    rodent.clear " :-( bye bye )-: "
+    sleep 1
+    # Protocol::Protocols.each {|protocol| protocol::terminate }/
+    exit
+  end
 end
