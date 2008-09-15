c2564e8c6cf5f384754ccb9c96e5f0cbb8c1fb91
elliottcable <git@elliottcable.name>
2008-09-15 14:58:58 -0800

    Protocols won't be kept in lib. Not sure how to handle them - for now, a new root directory.

--- lib/rat/protocol.rb
+++ lib/rat/protocol.rb
@@ -1,19 +1,20 @@
 module Rat
   module Protocol
-    @@initialized = []
-    def self.initialized
-      @@initialized
+    Protocols = []
+    def self.initialize
+      Protocols.each {|protocol| protocol::initialize }
     end
-  
-  
+    
+    Initialized = []
+    
     # To be inherited from for any +Protocol+. A +Rat+ +Protocol+ must expose at
     # least four things - an +::initialize+ method, a +::new+ (+#initialize+, in
     # other words) method, a +::terminate+ method, and a +#<<+ method.
     class Base
       def self.inherited klass
-      
+        Protocols << klass
       end
-    
+      
       # One of the required methods in a +Protocol+ plugin, +::initialize+ is
       # run when +Rat+ starts (if there is a default connection set up involving
       # your protocol), when the first window is created for a protocol, or when the
@@ -24,16 +25,16 @@ module Rat
       # This should take a login argument, and optionally a password argument
       # (if relevant), and should eventually call `super`.
       def self.initialize # login, password = nil
-        Protocol.initialized << self
+        Protocol::Initialized << self
       end
-    
+      
       # Provided by +Base+, and probably doesn't need to be modified - this
       # predicate will determine if your protocol has been globally initialized
       # by +Rat+ yet.
       def self.initialized?
         Protocol.initialized.include? self
       end
-    
+      
       # Required for every protocol, this method is run when +Rat+ closes down
       # itself, or, for some reason, closes the protocol.
       # 
@@ -41,7 +42,7 @@ module Rat
       def self.terminate
         Protocol.initialized.delete self
       end
-    
+      
       # The second required method, +::new+ (+#initialize+) is run when a window is
       # dedicated to a protocol. Should accept a window object (where incoming
       # messages or status information can be printed) and a target (that the user
@@ -53,7 +54,7 @@ module Rat
         @window = window
         @target = target
       end
-    
+      
       # Finally, a +#<<+ method that deals with outgoing messages, sending
       # them on their merry way or processing them in some way.
       #
@@ -63,22 +64,22 @@ module Rat
         @window << ['Rattagan', message].join(" > ")
       end
     end
-  
+    
     # This class applies to any window with no protocol.
     class None < Rat::Protocol::Base
       def self.initialize
         super
       end
-
+      
       def self.terminate
         super
       end
-
-
+      
+      
       def initialize window, target
         super window, target
       end
-
+      
       def << message
         super message
       end
--- lib/rat/protocols.rb
+++
@@ -1,1 +0,0 @@
-require 'rat/protocols/xmpp'
